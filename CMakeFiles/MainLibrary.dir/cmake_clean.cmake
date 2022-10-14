04d0cf1cae529e3ce9649c79fdba9b3df8e452d7
andrewwtpowell <powell.andrew26@gmail.com>
2022-10-14 16:24:56 -0400

    test updates

---
+++ CMakeFiles/MainLibrary.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MainLibrary.dir/main.cpp.o"
+  "CMakeFiles/MainLibrary.dir/src/board.cpp.o"
+  "CMakeFiles/MainLibrary.dir/src/group.cpp.o"
+  "CMakeFiles/MainLibrary.dir/src/piece.cpp.o"
+  "libMainLibrary.a"
+  "libMainLibrary.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MainLibrary.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/release/src/board.d
@@ -0,0 +1,10 @@
+build/release/src/board.o: src/board.cpp \
+ /home/apowell/projects/ProjectGO/include/board.h \
+ /home/apowell/projects/ProjectGO/include/group.h \
+ /home/apowell/projects/ProjectGO/include/piece.h
+
+/home/apowell/projects/ProjectGO/include/board.h:
+
+/home/apowell/projects/ProjectGO/include/group.h:
+
+/home/apowell/projects/ProjectGO/include/piece.h:
